08cb5f22ba1c8e16e0cb695c8003c56b281c3678
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-12-11 22:13:47 +0100

    Add LauncherFragment

---
+++ app/src/main/java/com/rradzzio/sessionmanager/ui/auth/LauncherFragment.kt
@@ -0,0 +1,14 @@
+package com.rradzzio.sessionmanager.ui.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.rradzzio.sessionmanager.R
+
+class LauncherFragment : Fragment(R.layout.fragment_launcher) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
