e52af73cc7b5146c2dbbac4fdf37b2ee8adfa753
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-12-11 21:50:37 +0100

    Add RegisterFragment

---
+++ app/src/main/java/com/rradzzio/sessionmanager/ui/auth/RegisterFragment.kt
@@ -0,0 +1,15 @@
+package com.rradzzio.sessionmanager.ui.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.rradzzio.sessionmanager.R
+
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
