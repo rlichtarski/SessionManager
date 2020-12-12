d8577642ac32dc8a5ad905c847d0ed4211e4dd5c
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-12-12 01:12:38 +0100

    Add auth_nav_graph and set up AuthActivity for it

--- app/src/main/java/com/rradzzio/sessionmanager/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/rradzzio/sessionmanager/ui/auth/AuthActivity.kt
@@ -2,12 +2,26 @@ package com.rradzzio.sessionmanager.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import com.rradzzio.sessionmanager.R
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity(), NavController.OnDestinationChangedListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findNavController(R.id.auth_nav_graph).addOnDestinationChangedListener(this)
+
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        //not implemented yet
     }
 }
