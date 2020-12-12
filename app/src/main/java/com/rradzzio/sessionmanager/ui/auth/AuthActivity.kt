fe8b52724857dae1006ea025329db31da7751dfe
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-12-12 16:34:25 +0100

    Navigate between auth fragments

--- app/src/main/java/com/rradzzio/sessionmanager/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/rradzzio/sessionmanager/ui/auth/AuthActivity.kt
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.rradzzio.sessionmanager.R
 
 class AuthActivity : AppCompatActivity(), NavController.OnDestinationChangedListener{
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
 
-        findNavController(R.id.auth_nav_graph).addOnDestinationChangedListener(this)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.auth_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
 
     }
 
--- app/src/main/java/com/rradzzio/sessionmanager/ui/auth/LauncherFragment.kt
+++ app/src/main/java/com/rradzzio/sessionmanager/ui/auth/LauncherFragment.kt
@@ -3,12 +3,37 @@ package com.rradzzio.sessionmanager.ui.auth
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.rradzzio.sessionmanager.R
+import com.rradzzio.sessionmanager.databinding.FragmentLauncherBinding
 
 class LauncherFragment : Fragment(R.layout.fragment_launcher) {
 
+    private lateinit var binding: FragmentLauncherBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLauncherBinding.bind(view)
+
+        binding.loginTxt.setOnClickListener {
+            navLogin()
+        }
+
+        binding.registerTxt.setOnClickListener {
+            navRegister()
+        }
+
+        binding.focusableView.requestFocus()
+
+
+    }
+
+    private fun navRegister() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
     }
 
 }
